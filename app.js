59dac4ec0cea6f5300a99669252a732432648869
Winnie Lin <winnie50431@gmail.com>
2021-11-15 00:07:31 +0800

    first commit

--- app.js
+++ app.js
@@ -31,11 +31,18 @@ const input = document.querySelector("input#keywords");
 const search = document.querySelector("#search");
 // blocks
 const cityTopic = document.querySelector(".topic_city");
+const topic = document.querySelector("section.topic");
+const allTopics = document.querySelectorAll("section.topic");
 // cards
-const mapCards = document.querySelector(".cards_map");
+const mapCards = document.querySelector("div.cards_map");
+const foodCards = document.querySelector("div.cards_food");
+const hotelCards = document.querySelector("div.cards_hotel");
+const activityCards = document.querySelector("div.cards_activity");
 // btn
 const prevMap = document.querySelector(".prev_map");
 const nextMap = document.querySelector(".next_map");
+
+let url, city, type, keywords, limitNum;
 
 /** 縣市列表 */
 function cityList() {
@@ -48,7 +55,7 @@ function cityList() {
 cityList();
 
 /** 熱門城市換頁 */
-prevMap.remove();
+if (prevMap) prevMap.remove();
 const toPrevMap = (e) => {
   console.log(e.target);
   prevMap.remove();
@@ -62,9 +69,291 @@ const toNextMap = (e) => {
   mapCards.style.right = "100%";
 };
 
+/** 預設顯示資料 */
+const setDefault = async () => {
+  let block;
+  /** 活動 */
+  if (activityCards) {
+    type = "Activity";
+    limitNum = 10;
+    url = `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}?$top=${limitNum}&$format=JSON`;
+  }
+  try {
+    let html = "";
+    const res = await fetch(url, {
+      headers: getAuthorizationHeader(),
+    });
+    const data = await res.json();
+    // console.log(data);
+    data.forEach((d) => {
+      // console.log(d.Name, d.Picture.PictureUrl1);
+      if (d.Picture.PictureUrl1) {
+        html += `
+      <div class="card_activity">
+      <div class="activity_img">
+        <img
+          src="${d.Picture.PictureUrl1}"
+          alt="${d.Name}"
+        />
+      </div>
+      <div class="activity_content">
+        <h4 class="activity_title">
+        ${d.Name}
+        </h4>
+        <p>
+        ${d.Description}...
+        </p>
+      </div>
+      <div class="info">
+        <span
+          ><i class="text_primary fas fa-map-marker-alt"></i
+          >&nbsp;${d.City}</span
+        >
+        <button class="btn btn_outline_primary">活動詳情</button>
+      </div>
+    </div>
+        `;
+      }
+    });
+    activityCards.innerHTML = html;
+  } catch (err) {
+    console.log(err);
+  }
+
+  /** 美食 */
+  if (foodCards) {
+    block = foodCards;
+    type = "Restaurant";
+    limitNum = 10;
+  }
+  try {
+    url = `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}?$top=${limitNum}&$format=JSON`;
+    let html = "";
+    const res = await fetch(url, {
+      headers: getAuthorizationHeader(),
+    });
+    const data = await res.json();
+    // console.log(data);
+    data.forEach((d) => {
+      // console.log(d.Name, d.Picture.PictureUrl1);
+      if (d.Picture.PictureUrl1) {
+        html += `
+        <div class="card">
+        <div class="img_container">
+          <img
+            src="${d.Picture.PictureUrl1}"
+            alt="${d.Name}"
+          />
+        </div>
+        <div class="card_content">
+          <p>${d.Name}</p>
+          <div>
+            <i class="text_primary fas fa-map-marker-alt"></i>
+            <span>${d.Address[0] + d.Address[1] + d.Address[2]}</span>
+          </div>
+        </div>
+      </div>
+          `;
+      }
+    });
+    block.innerHTML = html;
+  } catch (err) {
+    console.log(err);
+  }
+
+  /** 住宿 */
+  if (hotelCards) {
+    block = hotelCards;
+    type = "Hotel";
+    limitNum = 10;
+  }
+  try {
+    url = `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}?$top=${limitNum}&$format=JSON`;
+    let html = "";
+    const res = await fetch(url, {
+      headers: getAuthorizationHeader(),
+    });
+    const data = await res.json();
+    // console.log(data);
+    data.forEach((d) => {
+      // console.log(d.Name, d.Picture.PictureUrl1);
+      if (d.Picture.PictureUrl1) {
+        html += `
+        <div class="card">
+        <div class="img_container">
+          <img
+            src="${d.Picture.PictureUrl1}"
+            alt="${d.Name}"
+          />
+        </div>
+        <div class="card_content">
+          <p>${d.Name}</p>
+          <div>
+            <i class="text_primary fas fa-map-marker-alt"></i>
+            <span>${d.Address[0] + d.Address[1] + d.Address[2]}</span>
+          </div>
+        </div>
+      </div>
+          `;
+      }
+    });
+    block.innerHTML = html;
+  } catch (err) {
+    console.log(err);
+  }
+};
+setDefault();
+
+/** 篩選器
+ *  1. get the form data : eventListner
+ *  2. set URL : setURL()
+ *  3. fetch API
+ *  4. add element to layout
+ */
+const setURL = () => {
+  if (type && !city && !keywords) {
+    url = `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}?$top=40&$format=JSON`;
+  } else if (type && city && !keywords) {
+    url = `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}/${city}?$top=40&$format=JSON`;
+  } else if (type && !city && keywords) {
+    url = `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}?$filter=contains(Description,'${keywords}')&$top=40&$format=JSON`;
+  } else if (type && city && keywords) {
+    url = `https://ptx.transportdata.tw/MOTC/v2/Tourism/${type}/${city}?$filter=contains(Name,'${keywords}')&$top=40&$format=JSON`;
+  } else {
+    alert("請選擇類別");
+  }
+  console.log(url);
+};
+
+const getData = async (e) => {
+  let html = "";
+
+  /** 取得表單資料 */
+  e.preventDefault();
+  console.log(city, type, keywords);
+
+  /** 依類別取得資料 */
+  setURL();
+  try {
+    const res = await fetch(url, {
+      headers: getAuthorizationHeader(),
+    });
+    const data = await res.json();
+    console.log(data);
+    /** 清空多餘的標題 */
+    if (allTopics.length >= 1) {
+      for (let i = 1; i < allTopics.length; i++) {
+        console.log(i);
+        allTopics.item(i).remove();
+      }
+    }
+    /** 渲染標題 */
+    if (type === "Restaurant") {
+      html += `
+      <div class="topic_title">
+      <img src="./images/Icon/Rectangle.png" alt="" />
+      <h3>熱門美食</h3>
+    </div>
+    <div class="cards cards_food">
+        `;
+    } else if (type === "Hotel") {
+      html += `
+      <div class="topic_title">
+      <img src="./images/Icon/Rectangle.png" alt="" />
+      <h3>熱門住宿</h3>
+    </div>
+    <div class="cards cards_hotel">
+        `;
+    } else if (type === "Activity") {
+      html += `
+      <div class="topic_title">
+      <img src="./images/Icon/Triangle.png" alt="" />
+      <h3>熱門活動</h3>
+    </div>
+    <div class="cards cards_activity">
+      `;
+    }
+    /** 渲染內容 */
+    if (type === "Restaurant" || type === "Hotel") {
+      data.forEach((d) => {
+        if (d.Name && d.Picture.PictureUrl1) {
+          console.log(d.Name, d.Picture.PictureUrl1);
+          html += `
+          <div class="card">
+          <div class="img_container">
+            <img
+              src="${d.Picture.PictureUrl1}"
+              alt="${d.Name}"
+            />
+          </div>
+          <div class="card_content">
+            <p>${d.Name}</p>
+            <div>
+              <i class="text_primary fas fa-map-marker-alt"></i>
+              <span>${d.Address[0] + d.Address[1] + d.Address[2]}</span>
+            </div>
+          </div>
+        </div>
+        `;
+        }
+      });
+      html += `</div>`;
+      topic.innerHTML = html;
+    } else if (type === "Activity") {
+      data.forEach((d) => {
+        // console.log(d.Name, d.Picture.PictureUrl1);
+        if (d.Picture.PictureUrl1) {
+          html += `
+          <div class="card_activity">
+          <div class="activity_img">
+            <img
+              src="${d.Picture.PictureUrl1}"
+              alt="${d.Name}"
+            />
+          </div>
+          <div class="activity_content">
+            <h4 class="activity_title">
+            ${d.Name}
+            </h4>
+            <p>
+            ${d.Description}...
+            </p>
+          </div>
+          <div class="info">
+            <span
+              ><i class="text_primary fas fa-map-marker-alt"></i
+              >&nbsp;${d.City}</span
+            >
+            <button class="btn btn_outline_primary">活動詳情</button>
+          </div>
+        </div>
+          `;
+        }
+      });
+      html += `</div>`;
+      topic.className = "topic topic_activity";
+      topic.innerHTML = html;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 /** 監聽事件 */
+// 熱門城市換頁
 prevMap.addEventListener("click", toPrevMap);
 nextMap.addEventListener("click", toNextMap);
+// 篩選器(表單)
+typeSelect.addEventListener("change", (e) => {
+  type = e.target.value;
+});
+citySelect.addEventListener("change", (e) => {
+  city = e.target.value;
+});
+input.addEventListener("change", (e) => {
+  keywords = e.target.value;
+});
+search.addEventListener("click", getData);
 
 function getAuthorizationHeader() {
   let AppID = "246448c88bce42bfa303e9edd65e4c4f";
